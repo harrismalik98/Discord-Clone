9449b6c089e932c4166dcbb08da90d508886dfc7
harrismalik98 <mharrismalik98@gmail.com>
2024-09-03 00:09:25 +0500

    Prisma & Aiven setup

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,35 @@
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+const SetupPage = async() => {
+
+    const profile = await initialProfile();
+
+    if ('id' in profile)
+    {
+        const server = await db.server.findFirst({
+            where: {
+                members : {
+                    some: {
+                        profileId: profile.id
+                    }
+                }
+            }
+        });
+
+
+        if(server)
+        {
+            return redirect(`/servers/${server.id}`);
+        }
+    }
+
+    return ( 
+        <div>
+            <p>Create a Server</p>
+        </div>
+     );
+}
+ 
+export default SetupPage;
